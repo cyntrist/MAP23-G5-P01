6450b294c3256b58e70f4afe0e05589d6c1ab66e
danzhu26x <danizhu26@gmail.com>
2023-01-28 18:36:54 +0100

    Script LifeComponent a medias
    Si

---
+++ Assets/Scripts/LIfeComponent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeComponent : MonoBehaviour
+{
+    public static bool _death;
+    private float cd = 0.5f;
+    GameObject _enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _death = false;
+        _enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*  if (death)  //animacion de muerte
+          {
+              Animator.SetTrigger("Death");
+          } */
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            Debug.Log("muerto");
+        }
+    }
+
+
+
+}
