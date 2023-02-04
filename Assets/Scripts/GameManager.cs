0fabd89949cf1f6ef06199c1d4d93af74589c924
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-02-04 13:03:55 +0100

    GM + UIM mejor y más comentado
    como más gente va a tener que mirarlo, lo quiero dejar lo más claro posible

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public enum GameStates { START, INTRO, GAME, GAMEOVER };
-    public enum MarioStates { PEQUE, GRANDE, FUEGO, ESTRELLA };
+    public enum GameStates { START, INTRO, GAME, GAMEOVER };    // Estados del juego
+    public enum MarioStates { PEQUE, GRANDE, FUEGO, ESTRELLA }; // Estados de Mario
 
     #region references
-    private UIManager _UIManager;
-    [SerializeField] GameObject _level;
+    private UIManager _UIManager;           // Referencia al UIManager
+    [SerializeField] GameObject _level;     // Referencia al prefab del nivel 1-1
     #endregion
 
     #region properties
-    private static GameManager _instance;
-    private GameStates _currentState;
-    private GameStates _nextState;
-    private static MarioStates _marioState;
-    private float _remainingTime = 60;
-    private float _introTime = 3; // Segundos que dura la pantalla en negro al cargar el nivel
-    private int _lives = 3;
-    private int _score;
-    private int _coins;
-    public static GameManager Instance { get { return _instance; } }
+    private static GameManager _instance;   // Instancia privada del singleton
+    private GameStates _currentState;       // Estado actual del juego
+    private GameStates _nextState;          // Siguiente estado del juego
+    private static MarioStates _marioState; // Estado actual de Mario
+    private float _remainingTime = 60;      // Segundos que dura el nivel
+    private float _introTime = 3;           // Segundos que dura la pantalla en negro al cargar el nivel (INTRO state)
+    private int _lives = 3;                 // Vidas de Mario
+    private int _score;                     // Puntuación total en el nivel
+    private int _coins;                     // Monedas totales en el nivel
+    public static GameManager Instance { get { return _instance; } } // MÉTODOS GETTER
     public GameStates CurrentState { get { return _currentState; } }
     public static MarioStates MarioState { get { return _marioState; } }
     public int Lives { get { return _lives; } }
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour
     }
 
     // BLOQUE DE JUEGO 
-    public void OneUp() // champiñon verde / monedas
+    public void OneUp() // champiñon verde / 100 monedas
     {
         _lives++;
     }
@@ -66,57 +66,46 @@ public class GameManager : MonoBehaviour
         _marioState--;
     }
 
-    // BLOQUE DE MÁQUINA DE ESTADOS 
+    // BLOQUE DE MÁQUINA DE ESTADOS DEL JUEGO
     public void EnterState(GameStates newState)
     {
         switch (newState) // Diferentes comportamientos según estado al que se entra
         { // En sí, solo cambia el grupo de UI por cada estado y en GAME carga el nivel
-            case GameStates.START:
-                _UIManager.SetMenu(GameStates.START);
+            case GameStates.START:                       //     *MENÚ INICIAL*
+                _UIManager.SetMenu(GameStates.START);    // Activa menú inicial
                 break;
-            case GameStates.INTRO:
-                _UIManager.SetLives(_lives);
-                _UIManager.SetMenu(GameStates.INTRO);
+            case GameStates.INTRO:                       //     *INTRO* (Pantalla en negro con las vidas antes de cargar el lvl)
+                _UIManager.SetLives(_lives);             // Inicializa valores de vida en la UI
+                _UIManager.SetMenu(GameStates.INTRO);    // Activa menú intro
                 break;
-            case GameStates.GAME:
-                LoadLevel();
-                _UIManager.SetMenu(GameStates.GAME);
-                _UIManager.SetUpGameHUD(_remainingTime); // Inicializa el HUD
+            case GameStates.GAME:                        //     *JUEGO*
+                LoadLevel();                             // Instancia el nivel
+                _UIManager.SetUpGameHUD(_remainingTime); // Inicializa valores del HUD
+                _UIManager.SetMenu(GameStates.GAME);     // Activa HUD
                 break;
-            case GameStates.GAMEOVER:
-                UnloadLevel();
-                _UIManager.SetMenu(GameStates.GAMEOVER);
+            case GameStates.GAMEOVER:                    //     *FIN DEL JUEGO*
+                UnloadLevel();                           // Descarga el nivel
+                _UIManager.SetMenu(GameStates.GAMEOVER); // Activa el texto de GameOver
                 break;
         }
-        _currentState = newState; // Finaliza el cambio
+        _currentState = newState;                        // Finaliza el cambio
         Debug.Log("CURRENT: " + _currentState);
     }
 
-    /*
-    private void ExitState(GameStates newState)
-    {
-        if (newState == GameStates.GAME) 
-        {
-            //ª
-        }
-    }
-    */
-
     private void UpdateState(GameStates state)
     {
-        if (_currentState == GameStates.INTRO)
+        if (state == GameStates.INTRO) // INTRO: únicamente una cuenta atrás
         {
-            _introTime -= Time.deltaTime;
+            _introTime -= Time.deltaTime; // Cuenta atrás
             if (_introTime <= 0)
             {
-                _nextState = GameStates.GAME;
+                _nextState = GameStates.GAME; // Pasa a GAME
             }
         }
 
-        if (_currentState == GameStates.GAME) // En el resto de estados no hace falta nada de momento
+        if (state == GameStates.GAME) // GAME: cuenta atrás o vidas a 0 + actualizar HUD cada frame
         {
             _remainingTime -= Time.deltaTime; // Cuenta atrás
-
             if (_remainingTime < 0 || _lives <= 0) // Si se acaba el tiempo o las vidas
             {
                 _nextState = GameStates.GAMEOVER;
@@ -126,17 +115,17 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void RequestStateChange(GameStates newState)
+    public void RequestStateChange(GameStates newState) // Método público para cambiar el valor privado de estado 
     {
-        _nextState = newState;  // Método público para cambiar el valor privado de estado 
+        _nextState = newState;  
     }
 
-    private void LoadLevel()
+    private void LoadLevel() // Instancia el lvl y ya
     {
         Instantiate(_level, Vector3.zero, Quaternion.identity);
     }
 
-    private void UnloadLevel()
+    private void UnloadLevel() // Lo destruye y ya
     {
         Destroy(_level);
     }
@@ -150,16 +139,16 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         _currentState = GameStates.INTRO; // Valor dummy para que se realice el cambio nada más empezar
-        _nextState = GameStates.START; // Estado inicial, es diferente al current para que el EnterState del primer update se realice
-        _marioState = MarioStates.PEQUE; // Inicializandolo para el LifeComp
+        _nextState = GameStates.START;    // Estado inicial, es diferente al current para que el EnterState del primer update se realice
+        _marioState = MarioStates.PEQUE;  // Inicializandolo para el LifeComp
     }
 
     void Update()
     {
         if (_nextState != _currentState) // Si se requiere cambiar de estado (si current == next es que seguimos en el mismo)
         {
-            EnterState(_nextState); // Entramos al siguiente estado
+            EnterState(_nextState);      // Entramos al siguiente estado
         }
-        UpdateState(_currentState); // Update según el estado
+        UpdateState(_currentState);      // Update según el estado
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     #region references
-    [SerializeField] private TMP_Text _scoreTMP;
-    [SerializeField] private TMP_Text _coinsTMP;
-    [SerializeField] private TMP_Text _remainingTimeTMP;
-    [SerializeField] private TMP_Text _scoreMessageTMP; // para enseñar el score de las acciones
-    [SerializeField] private TMP_Text _introLivesTMP; // para enseñar las vidas
-    [SerializeField] private GameObject _mainMenu;
-    [SerializeField] private GameObject _introMessage; // para enseñar las vidas
-    [SerializeField] private GameObject _gameplayHUD;
-    [SerializeField] private GameObject _gameOverMenu;
+    [SerializeField] private TMP_Text _scoreTMP;         // GAMEHUD: textbox para enseñar la puntuación total
+    [SerializeField] private TMP_Text _coinsTMP;         // GAMEHUD: textbox para enseñar las monedas
+    [SerializeField] private TMP_Text _remainingTimeTMP; // GAMEHUD: textbox para enseñar el contrareloj
+    [SerializeField] private TMP_Text _scoreMessageTMP;  // GAMEHUD: textbox para enseñar el score de las acciones
+    [SerializeField] private TMP_Text _introLivesTMP;    // INTRO: textbox para enseñar las vidas
+    [SerializeField] private GameObject _mainMenu;       // START UI
+    [SerializeField] private GameObject _introMessage;   // INTRO UI
+    [SerializeField] private GameObject _gameplayHUD;    // GAME UI
+    [SerializeField] private GameObject _gameOverMenu;   // GAME OVER UI
     #endregion
 
     #region properties
-    private GameManager.GameStates _activeMenu;
-    private GameObject[] _menus;
+    private GameManager.GameStates _activeMenu;          // Menú actual
+    private GameObject[] _menus;                         // Array de menús totales
     #endregion
 
     #region methods
-    public void RequestStateChange(int newState)
+    public void RequestStateChange(int newState) // OnClick() del ButtonPrueba aunque ponga que tiene 0 referencias
     {
         GameManager.Instance.RequestStateChange((GameManager.GameStates)newState);
     }
 
-    public void UpdateGameHUD(float time)
+    public void UpdateGameHUD(float time) // Actualiza en cada frame los datos del HUD
     {
         _scoreTMP.text = (GameManager.Instance.Score).ToString("D6");
-        _coinsTMP.text = "x" + (GameManager.Instance.Coins).ToString("D2"); // +1 solo como debug para ver si funciona
+        _coinsTMP.text = "x" + (GameManager.Instance.Coins).ToString("D2");
         _remainingTimeTMP.text = "" + (int)time;
     }
 
-    public void SetUpGameHUD(float time) // Actualiza *sólo cada vez que se carga un nivel* los datos de ese nivel 
+    public void SetUpGameHUD(float time) // Inicializa el HUD a 0 + tiempo total
     {
         _scoreTMP.text = "000000";
         _coinsTMP.text = "x00";
@@ -42,8 +42,8 @@ public class UIManager : MonoBehaviour
         GameManager.Instance.AddScore(10); // para ver si funca
     }
 
-    public void SetMenu(GameManager.GameStates newMenu)
-    { // desactiva el menú anterior, actualiza el actual y lo activa
+    public void SetMenu(GameManager.GameStates newMenu)  // Desactiva el menú anterior, actualiza el actual y lo activa
+    {
         _menus[(int)_activeMenu].SetActive(false);
         Debug.Log("Desactivado: " + _activeMenu.ToString());
         _activeMenu = newMenu;
@@ -51,12 +51,13 @@ public class UIManager : MonoBehaviour
         Debug.Log("Activado: " + _activeMenu.ToString());
     }
 
-    public void SetLives(int lives)
+    public void SetLives(int lives) // Actualiza la cantidad de vidas en el estado INTRO
     {
         _introLivesTMP.text = "x  " + lives;
     }
 
-    public void ShowScore(int score, bool activate)
+    public void ShowScore(int score, bool activate) // TO-DO: que se active, salga encima de mario/en medio de la pantalla?
+                                                    // y suba hasta desactivarse pasado x tiempo
     {
         _scoreMessageTMP.text = score.ToString();
         _scoreMessageTMP.enabled = activate;
