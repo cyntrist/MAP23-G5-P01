7306515cc5bf398f17d6946667a6342f09c06801
Averfer <niceghoul52@gmail.com>
2023-02-09 16:28:49 +0100

    Koopa behaviour planteado (Falta solucionar los errores que hay)

--- Assets/Scripts/CPUMovementComponent.cs
+++ Assets/Scripts/CPUMovementComponent.cs
@@ -7,17 +7,12 @@ public class CPUMovementComponent : MonoBehaviour
     [SerializeField]
     private float _cpuSpeed = 1.0f;
     [SerializeField]
-    private float _rayDistance = 0.1f;
     public bool _derecha = false;
-    private RaycastHit2D _myRaycastHit;
-    private LayerMask _myLayerMask;
     // Start is called before the first frame update
     void Start()
     {
         _cpuTransform = transform;
         _cpuPosition = _cpuTransform.position;
-        _myRaycastHit = new RaycastHit2D();
-        _myLayerMask = LayerMask.GetMask("Player");
     }
 
     // Update is called once per frame
@@ -25,7 +20,8 @@ public class CPUMovementComponent : MonoBehaviour
     {
         if (_derecha == true) _cpuTransform.Translate(Vector2.right * _cpuSpeed * Time.deltaTime);
         else _cpuTransform.Translate(Vector2.left * _cpuSpeed * Time.deltaTime);
-        _myRaycastHit = Physics2D.Raycast(_cpuPosition, Vector2.up, _rayDistance, _myLayerMask);
-        if (_myRaycastHit.collider != null) Destroy(gameObject);
+    }
+    void OnCollisionEnter2D(Collision collision) { 
+        _derecha = !_derecha;
     }
 }
