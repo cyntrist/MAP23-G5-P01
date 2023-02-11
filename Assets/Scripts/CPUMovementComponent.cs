9b0907a464d8dc1c451d047293863b3811cd8478
danzhu26x <danizhu26@gmail.com>
2023-02-11 21:03:58 +0100

    poner enemigos en el mapa y correccion de su movimiento
    hay goombas q vuelan xD

--- Assets/Scripts/CPUMovementComponent.cs
+++ Assets/Scripts/CPUMovementComponent.cs
@@ -5,23 +5,53 @@ public class CPUMovementComponent : MonoBehaviour
     private Transform _cpuTransform;
     private Vector2 _cpuPosition;
     [SerializeField]
-    private float _cpuSpeed = 1.0f;
+    private float _cpuSpeed = 0.25f;
     [SerializeField]
-    public bool _derecha = false;
+    public bool _derecha;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         _cpuTransform = transform;
-        _cpuPosition = _cpuTransform.position;
+        rb = GetComponent<Rigidbody2D>();
+        //_cpuPosition = _cpuTransform.position;
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        if (_derecha == true) _cpuTransform.Translate(Vector2.right * _cpuSpeed * Time.deltaTime);
-        else _cpuTransform.Translate(Vector2.left * _cpuSpeed * Time.deltaTime);
+        if (_derecha)
+        {
+            _cpuTransform.Translate(2 * Time.deltaTime * _cpuSpeed, 0, 0);
+         
+        }
+        else
+        {
+            _cpuTransform.Translate(-2 * Time.deltaTime * _cpuSpeed,0,0);
+
+        }
+
+
     }
-    void OnCollisionEnter2D(Collision2D collision) { 
-        _derecha = !_derecha;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tube") || collision.gameObject.CompareTag("Enemy"))
+        {
+           
+            if (_derecha)
+            {
+                _derecha = false;
+            }
+            else
+            {
+                _derecha = true;
+
+            }
+            
+            transform.localScale= new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            
+        }
     }
 }
